d64b30474874974ec445569ce272cd3bc54a5c75
Leonhard Weiler <Laser_Leo@hotmail.com>
2025-12-24 13:04:26 +0100

    New power rule to limit to 85% added systemd services split into system and user

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@ set -e
 
 DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-# Prüfen, ob stow installiert ist
 if ! command -v stow &> /dev/null; then
     echo "Stow wird benötigt. Bitte zuerst installieren."
     exit 1
@@ -11,11 +10,9 @@ fi
 
 cd "$DOTFILES_DIR"
 
-# Pakete definieren
-ROOT_PKGS=("ly" "zram")       # Pakete, die root brauchen
-USER_PKGS=()                   # Normale Pakete automatisch erkennen
+ROOT_PKGS=("ly" "zram" "systemd-system")
+USER_PKGS=()
 
-# Alle Ordner als mögliche Pakete sammeln
 for dir in */; do
     dir=${dir%/}
     skip=false
@@ -28,19 +25,16 @@ for dir in */; do
     $skip || USER_PKGS+=("$dir")
 done
 
-# Bestätigung
 read -p "Möchtest du die Änderungen wirklich anwenden? [y/N]: " confirm
 confirm=${confirm,,}
 
 if [[ "$confirm" == "y" || "$confirm" == "yes" ]]; then
     echo "Apply changes..."
 
-    # Normale Pakete
     for pkg in "${USER_PKGS[@]}"; do
         stow -R -v "$pkg"
     done
 
-    # Root-Pakete
     for pkg in "${ROOT_PKGS[@]}"; do
         sudo stow -R -v --target=/ "$pkg"
     done
