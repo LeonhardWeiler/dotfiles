b9bab74471c5d5be4ba7e6a9340732c279653a5c
Leonhard Weiler <leonhard-weiler@hotmail.com>
2026-07-27 16:55:58 +0200

    tinylsp changes and keepassxc

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -32,6 +32,18 @@ return {
         },
       })
 
+      -- Ohne dies behandelt tinymist jede geoeffnete .typ-Datei als eigenes
+      -- Wurzeldokument, wodurch einzelne Kapitel Fehler wie fehlende Labels
+      -- oder fehlende heading-numbering melden. "lockDatabase" liest die
+      -- Datei tinymist.lock im Projekt-Root und analysiert Kapitel im
+      -- Kontext des dort eingetragenen Hauptdokuments. Existiert keine
+      -- tinymist.lock, faellt tinymist automatisch auf "singleFile" zurueck.
+      vim.lsp.config("tinymist", {
+        settings = {
+          projectResolution = "lockDatabase",
+        },
+      })
+
       vim.lsp.config("emmet_ls", {
         filetypes = {
           "html",
